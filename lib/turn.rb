dec573305dd950a22e10204182070fae6ea4e62b
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-11 01:14:01 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -52,3 +52,16 @@ def turn(board)
   move(board, index, current_player = "X")
   display_board(board)
 end
+
+# Alternate Solution
+# def turn(board)
+#   puts "Please enter 1-9:"
+#   input = gets.strip
+#   index = input_to_index(input)
+#   if valid_move?(board, index)
+#     move(board, index)
+#     display_board(board)
+#   else
+#     turn(board)
+#   end
+# end
